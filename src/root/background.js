a4a79e2c84c260f2337f24a90337f8096358491e
Sochiru <unleash9224@gmail.com>
2020-12-22 14:54:13 +0900

    AdBlock

--- src/root/background.js
+++ src/root/background.js
@@ -17,6 +17,7 @@ const checkBlockTarget = str =>
     '://ad.about.co.kr',
     '://neon.netinsight.co.kr',
     '://t1.daumcdn.net/kas/static/ba.min.js',
+    '://nstatic.dcinside.com/dgn/gallery/js/panda_tv.js',
     '://wcs.naver.net',
     '://cdn.taboola.com',
     '://securepubads.g.doubleclick.net'
