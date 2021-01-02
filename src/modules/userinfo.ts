5877ab4ca5abcf3c306df709f18db93f57eb09e4
Sochiru <unleash9224@gmail.com>
2021-01-03 00:00:28 +0900

    반고닉 ID 표시 추가

--- src/modules/userinfo.ts
+++ src/modules/userinfo.ts
@@ -40,12 +40,40 @@ export default {
       elem.dataset.refresherIp = ip_data && ip_data.name && format
     }
 
+    let IdInfoAdd = (elem: HTMLElement) => {
+      if (!elem || !elem.dataset.uid || elem.dataset.refresherId) return false
+
+      let img = elem.querySelector('img')
+      if (!img || img.src.indexOf('dc/w/images/nik.gif') === -1) {
+        return false
+      }
+
+      let text = document.createElement('span')
+      text.className = 'ip refresherIP'
+      text.innerHTML = `<span>(${elem.dataset.uid})</span>`
+      text.title = elem.dataset.uid
+
+      let fl = elem.querySelector('.fl')
+      if (fl) {
+        let flIpQuery = fl.querySelector('.ip')
+
+        if (flIpQuery) {
+          fl.insertBefore(text, flIpQuery.nextSibling)
+        }
+      } else {
+        elem.appendChild(text)
+      }
+
+      elem.dataset.refresherId = 'true'
+    }
+
     const elemAdd = (elem: HTMLElement | Document) => {
       let list = elem.querySelectorAll('.ub-writer')
       let iter = list.length
 
       while (iter--) {
         ipInfoAdd(list[iter] as HTMLElement)
+        IdInfoAdd(list[iter] as HTMLElement)
       }
 
       // TODO : 글목록 반고닉 사용자 ID 표시하기
@@ -53,6 +81,7 @@ export default {
 
     this.memory.uuid = filter.add('.ub-writer', (elem: HTMLElement) => {
       ipInfoAdd(elem)
+      IdInfoAdd(elem)
     })
     this.memory.uuid2 = eventBus.on('refresh', elemAdd)
 
