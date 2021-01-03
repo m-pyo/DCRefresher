4983f094219b0f7fbb606d048f133ab6de9ba777
Sochiru <unleash9224@gmail.com>
2021-01-03 23:46:52 +0900

    2.0.4, 빌드 옵션 수정

--- webpack.config.js
+++ webpack.config.js
@@ -8,127 +8,136 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 
-const dev = process.env.NODE_ENV !== 'production'
 const pkg = JSON.parse(fs.readFileSync('./package.json'))
 
-const options = {
-  entry: {
-    refresher: ['babel-polyfill', path.resolve('src', 'index.ts')]
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  },
-  module: {
-    rules: [
-      {
-        exclude: /(node_modules|_old_src|(sa|sc|c)ss|background)/,
-        test: /\.js|\.ts$/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env, argv) => {
+  let devMode = argv.mode === 'development'
+
+  const options = {
+    entry: {
+      refresher: ['babel-polyfill', path.resolve('src', 'index.ts')]
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    },
+    module: {
+      rules: [
+        {
+          exclude: /(node_modules|_old_src|(sa|sc|c)ss|background)/,
+          test: /\.js|\.ts$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-typescript']
+            }
+          }
+        },
+        {
+          exclude: /\.js|\.ts|\.woff2/,
+          test: /\.(sa|sc|c)ss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(ico|png|jpg|jpeg)?$/,
+          loader: 'file-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-typescript']
+            name: '[hash].[ext]'
+          }
+        },
+        {
+          test: /\.json?$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        },
+        {
+          test: /\.pug$/,
+          loader: 'pug-loader',
+          options: {
+            globals: {
+              RefresherVersion: pkg.version || '1.0.0',
+              RefresherDevMode: devMode
+            }
           }
         }
-      },
-      {
-        exclude: /\.js|\.ts|\.woff2/,
-        test: /\.(sa|sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(ico|png|jpg|jpeg)?$/,
-        loader: 'file-loader',
-        options: {
-          name: '[hash].[ext]'
-        }
-      },
-      {
-        test: /\.json?$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]'
-        }
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          globals: {
-            RefresherVersion: pkg.version || '1.0.0'
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'refresher.bundle.css'
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'src/manifest.json',
+            transform: (content, _p) => {
+              return Buffer.from(
+                JSON.stringify({
+                  description: pkg.description,
+                  version: pkg.version,
+                  ...JSON.parse(content.toString())
+                })
+              )
+            }
           }
+        ]
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'src/assets',
+            to: 'assets/'
+          }
+        ]
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'src/root',
+            to: './',
+            filter: async path => {
+              if (devMode) {
+                return path.indexOf('/vue.min.js') === -1
+              }
+
+              return path.indexOf('/vue.js') === -1
+            }
+          }
+        ]
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/views/index.pug',
+        filename: 'views/index.html',
+        inject: false,
+        templateParameters: {
+          RefresherVersion: pkg.version || '1.0.0',
+          RefresherDevMode: devMode
         }
+      })
+      // new BundleAnalyzerPlugin()
+    ],
+    resolve: {
+      extensions: ['.js', '.ts', '.css'],
+      modules: ['node_modules'],
+      alias: {
+        vue: 'vue/dist/vue.js'
       }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'refresher.bundle.css'
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/manifest.json',
-          transform: (content, _p) => {
-            return Buffer.from(
-              JSON.stringify({
-                description: pkg.description,
-                version: pkg.version,
-                ...JSON.parse(content.toString())
-              })
-            )
-          }
-        }
-      ]
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/assets',
-          to: 'assets/'
-        }
-      ]
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/root',
-          to: './'
-        }
-      ]
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/views/index.pug',
-      filename: 'views/index.html',
-      inject: false,
-      templateParameters: {
-        RefresherVersion: pkg.version || '1.0.0'
-      }
-    })
-    // new BundleAnalyzerPlugin()
-  ],
-  resolve: {
-    extensions: ['.js', '.ts', '.css'],
-    modules: ['node_modules'],
-    alias: {
-      vue: 'vue/dist/vue.js'
     }
   }
-}
 
-module.exports = (env, argv) => {
   options.mode = argv.mode
 
-  if (argv.mode === 'development') {
+  if (devMode) {
     options.devtool = 'eval-source-map'
-  }
-
-  if (argv.mode === 'production') {
+  } else {
+    devMode = false
     options.resolve.alias['vue'] = 'vue/dist/vue.min.js'
 
     delete options.devServer
