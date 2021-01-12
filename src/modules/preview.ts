681a4b24182b56de47b9a801b5e96b99e4a80a68
Sochiru <unleash9224@gmail.com>
2021-01-12 21:54:16 +0900

    #39 write_div의 width 값을 maxWidth로 수정

--- src/modules/preview.ts
+++ src/modules/preview.ts
@@ -960,9 +960,17 @@ let parse = (id: string, body: string) => {
   let downvotes = dom.querySelector('div.btn_recommend_box.clear .down_num')
     ?.innerHTML
 
-  let contents = dom.querySelector(
+  let content_query = dom.querySelector(
     '.view_content_wrap > div > div.inner.clear > div.writing_view_box'
-  )?.innerHTML
+  )
+
+  let writeDiv = content_query?.querySelector('.write_div') as HTMLElement
+  if (writeDiv && writeDiv.style.width) {
+    let width = writeDiv.style.width
+    writeDiv.style.width = 'unset'
+    writeDiv.style.maxWidth = width
+  }
+  let contents = content_query?.innerHTML
 
   let commentId = body
     .match(ISSUE_ZOOM_ID)![0]
@@ -1066,7 +1074,8 @@ export default {
     },
     colorPreviewLink: {
       name: '게시글 URL 변경',
-      desc: '미리보기를 열면 게시글의 URL을 변경하여 브라우저 탐색으로 게시글을 바꿀 수 있게 해줍니다.',
+      desc:
+        '미리보기를 열면 게시글의 URL을 변경하여 브라우저 탐색으로 게시글을 바꿀 수 있게 해줍니다.',
       default: true,
       type: 'check'
     },
