6580ca70e1118af8c8336c8323d1c63b9f3393a9
Seohyun Lee <04seohyun@gmail.com>
2021-01-14 15:34:29 +0900

    Fix design for user component

--- src/components/user.ts
+++ src/components/user.ts
@@ -1,10 +1,12 @@
 import { eventBus } from '../core/eventbus'
 
 export default {
-  template: `<div class="refresher-user" :data-me="me" v-on:click="clickHandle" :class="{cursor: !!this.user.id}" v-on:contextmenu="contextMenu">
-    <span class="refresher-user-icon" :data-icon="user.icon" :data-type="user.type"></span>
-    <span class="refresher-user-nick">{{user.nick}}</span>
-    <span class="refresher-user-info">{{user.id ? '(' + user.id + ')' : user.ip ? '(' + user.ip + (user.ip_data ? ', ' + (user.ip_data.length > 100 ? user.ip_data.substring(0, 97) + '...' : user.ip_data) : '') + ')' : ''}}</span>
+  template: `<div class="refresher-user" :data-me="me" v-on:click="clickHandle" :class="{cursor: !!this.user.id}" v-on:contextmenu="contextMenu" :title="(user.id ? '(' + user.id + ')' : user.ip ? '(' + user.ip + (user.ip_data ? ', ' + (user.ip_data.length > 100 ? user.ip_data.substring(0, 97) + '...' : user.ip_data) : '') + ')' : '').replaceAll('\\n', ' ').replaceAll(/  +/g, ' ')">
+    <div class="refresher-user-content">
+      <span class="refresher-user-icon" :data-icon="user.icon" :data-type="user.type"></span>
+      <span class="refresher-user-nick">{{user.nick}}</span>
+      <span class="refresher-user-info">{{user.id ? '(' + user.id + ')' : user.ip ? '(' + user.ip + (user.ip_data ? ', ' + (user.ip_data.length > 100 ? user.ip_data.substring(0, 97) + '...' : user.ip_data) : '') + ')' : ''}}</span>
+    </div>
   </div>`,
   props: {
     user: {
